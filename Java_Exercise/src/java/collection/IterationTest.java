1e81581d1399a3efc912ed86a0f4b198a132ca97
ethan-cao <ethancaocgx@gmail.com>
2018-04-28 12:18:59 +0200

    clean

--- Java_Exercise/src/java/collection/IterationTest.java
+++ Java_Exercise/src/java/collection/IterationTest.java
@@ -6,34 +6,35 @@ import java.util.List;
 
 public class IterationTest {
 
+	List<Integer>  a = new ArrayList<>();
+
 	public static void main(String[] args) {
-		// test 1 and test 2 are more efficient
+		// test 1 and test 2 are more efficient than test 3
 		// test 1 and test 2 are, performance wise, pretty much the same
 	}
 
 	void test1 (){
-		List<Integer>  a = new ArrayList<Integer>();
 		for (Integer integer : a) {
 		  integer.toString();
 		}
-	
 	}
 
 	// Order relies on iterator
 	void test2 (){
-		List<Integer>  a = new ArrayList<Integer>();
-		Integer integer = null;
-		for (Iterator iterator = a.iterator(); iterator.hasNext();) {
-		   integer = (Integer) iterator.next();
-		   integer.toString();
+		for (Iterator<Integer> iterator = a.iterator(); iterator.hasNext();) {
+		   Integer i = iterator.next();
+		}
+
+		// Alternative
+		Iterator<Integer> it = a.iterator();
+		while (it.hasNext()){
+			Integer i = it.next();
 		}
 	}
 	
 	void test3(){
-		List<Integer>  a = new ArrayList<Integer>();
 		for(int i = 0; i < a.size(); i++) {
-			Integer o = (Integer) a.get(i);
-			o.toString();
+			Integer o = a.get(i);
 		}
 	}
 }
