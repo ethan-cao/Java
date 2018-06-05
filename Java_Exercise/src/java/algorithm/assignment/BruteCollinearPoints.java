ac3bd1b268209c00ba8a84fd1ac41d16c6183d7b
ethan-cao <ethancaocgx@gmail.com>
2018-06-05 08:44:55 +0000

    sync

---
+++ Java_Exercise/src/java/algorithm/assignment/BruteCollinearPoints.java
@@ -0,0 +1,17 @@
+package algorithm.assignment;
+
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points){
+
+    }
+
+    public int numberOfSegments(){
+
+    }
+
+    public LineSegment[] segments(){
+
+    }
+}
+
+
---
+++ Java_Exercise/src/java/algorithm/assignment/LineSegment.java
@@ -0,0 +1,34 @@
+package algorithm.assignment;
+
+import java.awt.*;
+
+public class LineSegment {
+    private final Point p;
+    private final Point q;
+
+    public static void main(String[] args){
+    }
+
+    public LineSegment(Point p, Point q){
+        if (p == null || q == null) {
+            throw new NullPointerException("argument is null");
+        }
+
+        this.p = p;
+        this.q = q;
+    }
+
+    public void draw(){
+        p.drawTo(q);
+    }
+
+    @Override
+    public int hashCode(){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String toString(){
+        return p + " -> " + q;
+    }
+}
