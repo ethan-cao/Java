937ea17ba50150dfdacfaab9eb935f210237ef73
ethan-cao <ethancaocgx@gmail.com>
2019-01-16 20:25:07 +0000

    DP solution

--- Java_Exercise/src/java/leetCode/DP_Array_746.java
+++ Java_Exercise/src/java/leetCode/DP_Array_746.java
@@ -4,7 +4,8 @@ package leetCode;
 On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
 
 Once you pay the cost, you can either climb 1 or 2 steps. You need to find minimum cost to reach the top of the floor
-(top of the floor : the next place after the last element in cost[])
+(reach n-th floor : go beyond n-th element) !!!
+(top of the floor : go beyond the last element)  !!!
 and you can either start from the step with index 0 or 1.
 
 Example 1:
@@ -20,65 +21,78 @@ Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3]
 Note:
 cost will have a length in the range [2, 1000].
 Every cost[i] will be an integer in the range [0, 999].
+
 -------------------------
-#Input
+
+### Input
 int[] cost
 
-#Output
+### Output
 int cost
 
-#Example
+### Example
 [10, 15, 20] -> 15
 Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
 
-#Condition
+### Condition
 2 < cost[].length < 1000
 0 < cost[i] < 999
 
-#Essential problem  / Sub problem
+### Essential problem  / Sub problem
 DP
-subProblem
 
-#Solution (algorithm + data structure)
+======== minCostClimbingStairs1
+(reach n-th floor : go beyond n-th element) !!!
+cost[i] : the minimal cost to reach i-th floor
+cost[i]  = costs[i] + min(cost[i-1], cost[i-2])
 
-#Corner case
+cost[0] = costs[0]
+cost[1] = costs[1]
+
+L = costs.length
+reach the top floor : min(cost[L-1], cost[L-2])
+
+
+======== minCostClimbingStairs2
+cost[i] : the minimal cost to reach i-th floor
+cost[i] = costs[i] + min(cost[i-1])
+
+
+### Corner case
 
 */
 
 public class DP_Array_746 {
     public static void main(String[] args) {
-//        int[] costs = {10, 15, 20};
+        int[] costs = {10, 15, 20};
 //        int[] costs = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
-        int[] costs = {0, 1, 2, 2};
+//        int[] costs = {0, 1, 2, 2};
 
         int minCost = minCostClimbingStairs1(costs);
+        minCost = minCostClimbingStairs2(costs);
 
         System.out.println(minCost);
     }
 
-    public static int minCostClimbingStairs1(int[] costs) {
-        int minCost = Integer.MAX_VALUE;
+    private static int minCostClimbingStairs1(int[] costs) {
         int L = costs.length;
+        int[] cost = new int[L]; // minimal cost to reach i-th floor
 
-        for (int startIndex = 0; startIndex < 2; ++startIndex) {
-            int cost = 0;
-            int currentIndex = startIndex;
+        cost[0] = costs[0];
+        cost[1] = costs[1];
 
-            while (currentIndex < L) {
-                cost += costs[currentIndex];
-
-                if (currentIndex + 1 >= L || currentIndex + 2 >= L ) {
-                    break;
-                }
-
-                int stepToMove = costs[currentIndex + 2] > costs[currentIndex + 1] ? 1 : 2;
-
-                currentIndex += stepToMove;
-            }
-
-            minCost = Math.min(minCost, cost);
+        for (int i = 2 ; i < L ; ++i){
+            cost[i] = costs[i] + Math.min(cost[i-1], cost[i-2]);
         }
 
-        return minCost;
+        return Math.min(cost[L-1], cost[L-2]);
+    }
+
+    // simplified version of minCostClimbingStairs2
+    private static int minCostClimbingStairs2(int[] costs) {
+        for (int i = 2; i < costs.length; i++) {
+            costs[i] += Math.min(costs[i-1], costs[i-2]);
+        }
+        return Math.min(costs[costs.length-1], costs[costs.length-2]);
     }
 }
