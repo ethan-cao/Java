e7b26011ed429570b4ce3b2ac4b183adbcf247a8
ethan-cao <ethancaocgx@gmail.com>
2019-01-22 21:29:58 +0000

    done 303

--- Java_Exercise/src/java/leetCode/Array_26.java
+++ Java_Exercise/src/java/leetCode/Array_26.java
@@ -3,15 +3,14 @@ package leetCode;
 import java.util.HashSet;
 import java.util.Set;
 
-class Solution26{
+public class Array_26{
     public static void main(String[] args){
-    	Solution26 solution  = new Solution26();
 
     	int[] nums = {0,0,1,1,1,2,2,3,3,4};
-    	solution.removeDuplicates(nums);
+    	removeDuplicates(nums);
 	}
 
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
 	   	Set<Integer> uniqueValues = new HashSet<>();
 
 		for (int i = 0; i < nums.length; ++i){
