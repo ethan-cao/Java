34052f64f44bc5ecde3d0643256322720fdfb45e
ethan-cao <ethancaocgx@gmail.com>
2019-01-22 21:36:32 +0000

    sync

---
+++ Java_Exercise/src/java/leetCode/Sort_String_242.java
@@ -0,0 +1,41 @@
+package leetCode;
+
+/*
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+
+### Input
+String s, String t
+
+### Output
+boolean
+
+### Example
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Input: s = "rat", t = "car"
+Output: false
+
+### Condition
+You may assume the string contains only lowercase alphabets.
+
+### Essential problem
+
+### Corner case
+
+*/
+
+
+public class Sort_String_242 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        boolean result = false;
+
+        return result;
+    }
+}
