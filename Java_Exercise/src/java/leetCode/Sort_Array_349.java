120ecace5484ae3d2fcff20b8ecee8a364e08a9c
ethan-cao <ethancaocgx@gmail.com>
2019-01-29 17:26:57 +0000

    add 350

--- Java_Exercise/src/java/leetCode/Sort_Array_349.java
+++ Java_Exercise/src/java/leetCode/Sort_Array_349.java
@@ -1,17 +1,9 @@
 package leetCode;
 
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
-import edu.princeton.cs.algs4.In;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 
 /*
---
+++ Java_Exercise/src/java/leetCode/Sort_Array_350.java
@@ -0,0 +1,30 @@
+package leetCode;
+
+/*
+Given two arrays, write a function to compute their intersection.
+
+### Input
+int[] nums1, int[] nums2
+
+### Output
+int[]
+
+### Example
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+
+### Condition
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+
+
+### Essential problem
+
+### Corner case
+
+*/
+public class Sort_Array_350 {
+}
