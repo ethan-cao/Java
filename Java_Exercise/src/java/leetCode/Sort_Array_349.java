72a3df305bb30115f28faa7c4cb5298d026f8a46
ethan-cao <ethancaocgx@gmail.com>
2019-03-03 12:37:11 +0000

    better solution to 350

--- Java_Exercise/src/java/leetCode/Sort_Array_349.java
+++ Java_Exercise/src/java/leetCode/Sort_Array_349.java
@@ -1,6 +1,5 @@
 package leetCode;
 
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
--- Java_Exercise/src/java/leetCode/Sort_Array_350.java
+++ Java_Exercise/src/java/leetCode/Sort_Array_350.java
@@ -1,7 +1,5 @@
 package leetCode;
 
-import edu.princeton.cs.algs4.In;
-
 import java.util.*;
 
 /*
@@ -38,19 +36,23 @@ The result can be in any order.
 public class Sort_Array_350 {
 
     public static void main(String[] args) {
-//        int[] nums1 = {1, 2, 2, 1};
-//        int[] nums2 = {2, 2};
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+        // output [2, 2]
 
 //        int[] nums1 = {4, 9, 5};
 //        int[] nums2 = {9, 4, 9, 8, 4};
+        // output [4, 9]
 
 //        int[] nums1 = {1, 1};
 //        int[] nums2 = {1, 2, 1, 1, 1};
+        // output [1, 1]
 
-        int[] nums1 = {9, 8, 8};
-        int[] nums2 = {9, 4, 9, 8, 4};
+//        int[] nums1 = {9, 8, 8, 9, 8};
+//        int[] nums2 = {9, 4, 9, 8, 4};
+        // output [9, 9, 8]
 
-        System.out.println(Arrays.toString(intersection(nums1, nums2)));
+        System.out.println(Arrays.toString(intersection1(nums1, nums2)));
     }
 
     public static int[] intersection(int[] nums1, int[] nums2) {
@@ -71,10 +73,10 @@ public class Sort_Array_350 {
 
         List<Integer> result = new ArrayList<>();
 
-        for (int i = 0 ; i < listNew1.size(); ++i){
+        for (int i = 0; i < listNew1.size(); ++i) {
             Integer number = listNew1.get(i);
 
-            if (listNew2.contains(number)){
+            if (listNew2.contains(number)) {
                 result.add(number);
                 listNew2.remove(listNew2.indexOf(number));
             }
@@ -84,8 +86,39 @@ public class Sort_Array_350 {
     }
 
     public static int[] intersection1(int[] nums1, int[] nums2) {
+        if (null == nums1 || null == nums2 || 0 == nums1.length || 0 == nums2.length) {
+            return new int[0];
+        }
 
-        return new int[1];
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int idx1 = 0;
+        int idx2 = 0;
+
+        int l = Math.min(nums1.length, nums2.length);
+        int idx3 = 0;
+
+        int[] intersection = new int[l];
+        while (idx1 < nums1.length && idx2 < nums2.length) {
+
+            if (nums1[idx1] == nums2[idx2]) {
+                intersection[idx3] = nums1[idx1];
+                idx1++;
+                idx2++;
+                idx3++;
+            } else {
+                if (nums1[idx1] > nums2[idx2]) {
+                    idx2++;
+                } else {
+                    idx1++;
+                }
+            }
+        }
+
+        int[] result = new int[idx3];
+        System.arraycopy(intersection, 0, result, 0, idx3);
+
+        return result;
     }
-
 }
