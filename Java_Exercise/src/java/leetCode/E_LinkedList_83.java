4ee62f2cd57ae7b92dc81f6a0ccee024e1bc5a2e
ethan-cao <ethancaocgx@gmail.com>
2019-03-24 09:10:27 +0100

    update

--- Java_Exercise/src/java/leetCode/E_LinkedList_83.java
+++ Java_Exercise/src/java/leetCode/E_LinkedList_83.java
@@ -15,11 +15,37 @@ Given a sorted linked list, delete all duplicates such that each element appear 
 
 */
 
+import edu.princeton.cs.algs4.In;
+
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class E_LinkedList_83 {
     public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(2);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(3);
+        ListNode node6 = new ListNode(4);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+
+        deleteDuplicates(node1);
+        ListNode node = node1;
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
     }
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
@@ -29,6 +55,51 @@ public class E_LinkedList_83 {
     }
 
     public static ListNode deleteDuplicates(ListNode head) {
-        return null;
+        Set<Integer> set = new LinkedHashSet<>();
+
+        ListNode node = head;
+        while (node != null) {
+            set.add(node.val);
+            node.next = node;
+        }
+
+        node = head;
+        set.remove(head.val);
+
+        for (Integer i : set){
+            node.next = new ListNode(i);
+            node = node.next;
+        }
+
+        return node;
+    }
+
+    public static ListNode deleteDuplicates1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Set<Integer> set = new HashSet<>();
+        set.add(head.val);
+
+        ListNode node = head;
+        // manually add a duplicate value to the end
+
+        while (node != null && node.next != null) {
+
+            if (set.contains(node.next.val)) {
+                node.next = node.next.next;
+            } else {
+                set.add(node.next.val);
+            }
+
+            node = node.next;
+        }
+
+        if (node != null && set.contains(node.val)) {
+
+        }
+
+        return head;
     }
 }
