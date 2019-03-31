0742977bad905d8b1fa416ef4c531d39ddd7ba9a
ethan-cao <ethancaocgx@gmail.com>
2019-03-31 15:06:40 +0000

    168 done

--- Java_Exercise/src/java/leetCode/E_Recursion_Math_168.java
+++ Java_Exercise/src/java/leetCode/E_Recursion_Math_168.java
@@ -23,28 +23,52 @@ For example:
 ### Condition
 
 ### Essential problem
-it is NOT convert
+Convert decimal number to base 26 number
+
+1    -> A     1  * 26^0
+26   -> Z     26 * 26^0
+
+27   -> AA    1 * 26^1 + 1 * 26^0
+52   -> AZ    1 * 26^1 + 26 * 26^0
+53   -> BA    2 * 26^1 + 1 * 26^0
+78   -> BZ    2 * 26^1 + 26 * 26^0
+702  -> ZZ    26 * 26^1 + 26 * 26^0h
+
+703  -> AAA     1 * 26^2 + 1 * 26^1 + 1 * 26^0
+18278  -> ZZZ   26 * 26^2 + 26 * 26^1 + 26 * 26^0
+
+18279  -> AAAA   1 * 26^3 + 1 * 26^2 + 1 * 26^1 + 1 * 26^0
+475254 -> ZZZZ   26 * 26^3 + 26 * 26^2 + 26 * 26^1 + 26 * 26^0
+
+475255 -> AAAAA  1 * 26^4 + 1 * 26^3 + 1 * 26^2 + 1 * 26^1 + 1 * 26^0
 
 ### Corner case
 
 */
 
 public class E_Recursion_Math_168 {
+
     public static void main(String... args) {
-        System.out.println(convertToTitle(2));
-        System.out.println(convertToTitle(26));
-        System.out.println(convertToTitle(28));
-        System.out.println(convertToTitle(701));
+        System.out.println(convertToTitle(1));  // A
+        System.out.println(convertToTitle(2));  // B
+        System.out.println(convertToTitle(26));  // Z
+        System.out.println(convertToTitle(28));   // AB
+        System.out.println(convertToTitle(701));   // ZY
+        System.out.println(convertToTitle(731));    // ABC
     }
 
     public static String convertToTitle(int n) {
-        if (n <= 0) {
-            return "";
+        StringBuilder title = new StringBuilder();
+
+        while (n > 0) {
+            // Instead of 1 -> A, 26 -> Z, we can assume 0 -> A, 25 -> Z
+            n--;
+
+            title.insert(0, (char) (n % 26 + 'A'));
+
+            n /= 26;
         }
 
-
-
-        return "";
+        return title.toString();
     }
-
 }
