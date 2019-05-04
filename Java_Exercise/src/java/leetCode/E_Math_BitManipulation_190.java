db40963c70585ebec1f4018021c32e5b239253ae
ethan-cao <ethancaocgx@gmail.com>
2019-05-04 14:25:03 +0000

    update

--- Java_Exercise/src/java/leetCode/E_Math_BitManipulation_190.java
+++ Java_Exercise/src/java/leetCode/E_Math_BitManipulation_190.java
@@ -37,5 +37,36 @@ public class E_Math_BitManipulation_190 {
     }
 
     public static int reverseBits(int n) {
+        int result = 0;
+
+        // 32: because int is 32 bit
+        for (int i = 0; i < 32; ++i) {
+            int bit = n & 0b1;
+            result += bit;
+            n = n >>> 1;  // n >>>= 1;
+
+            // except the last digit, other digits after getting revered, need to be left shifted.
+            // so the next round can add the digit in right most position
+            if (i < 31) {
+                result = result << 1;
+            }
+        }
+
+        return result;
+    }
+
+    /*
+    for 8 bit binary number abcdefgh, the process is as follow:
+    abcdefgh -> efghabcd -> ghefcdab -> hgfedcba
+
+     O(log sizeof(int)
+     */
+    public static int reverseBits1(int n) {
+        n = (n >> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
     }
 }
