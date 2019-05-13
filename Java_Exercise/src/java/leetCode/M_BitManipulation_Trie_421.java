8e47dc9399155616cb5280650de0207d552cc1f3
ethan-cao <ethancaocgx@gmail.com>
2019-05-13 13:53:58 +0000

    update

---
+++ Java_Exercise/src/java/leetCode/M_BitManipulation_Trie_421.java
@@ -0,0 +1,42 @@
+package leetCode;
+
+/*
+Given a non-empty array of numbers, a0, a1, a2, … , an-1, where 0 ≤ ai < 231.
+Find the maximum result of ai XOR aj, where  0 ≤ i,j < n.
+
+Could you do this in O(n) runtime?
+
+### Example
+[3, 10, 5, 25, 2, 8] -> 28
+Explanation: The maximum result is 5 ^ 25 = 28.
+
+### Condition
+
+### Essential problem
+
+### Corner case
+
+*/
+
+
+public class M_BitManipulation_Trie_421 {
+
+    public static void main(String[] args) {
+        int[] a = {3, 10, 5, 25, 2, 8};
+
+        System.out.println(findMaximumXOR(a));
+    }
+
+    public static int findMaximumXOR(int[] nums) {
+        return 0;
+    }
+
+    // Bit manipulation
+    public static int findMaximumXOR1(int[] nums) {
+        int maxXOR = 0;
+
+
+
+        return maxXOR;
+    }
+}
--- Java_Exercise/src/java/leetCode/M_Trie_676.java
+++ Java_Exercise/src/java/leetCode/M_Trie_676.java
@@ -1,28 +1,7 @@
 package leetCode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class M_Trie_676 {
     public static void main(String[] args){
-
-    	int[] nums = {0,0,1,1,1,2,2,3,3,4};
-    	removeDuplicates(nums);
 	}
 
-    public static int removeDuplicates(int[] nums) {
-	   	Set<Integer> uniqueValues = new HashSet<>();
-
-		for (int i = 0; i < nums.length; ++i){
-			int number = nums[i];
-
-			if (!uniqueValues.contains(number)){
-				nums[uniqueValues.size()] = number;
-			}
-
-			uniqueValues.add(number);
-		}
-
-		return uniqueValues.size();
-    }
 }
