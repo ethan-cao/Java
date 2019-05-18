a4253d18c0f159585c0896fc8f7068380c52c2a8
ethan-cao <ethancaocgx@gmail.com>
2019-05-18 14:24:51 +0200

    update

--- Java_Exercise/src/java/algorithm/search/Trie.java
+++ Java_Exercise/src/java/algorithm/search/Trie.java
@@ -1,18 +1,7 @@
 package algorithm.search;
 
-import java.util.Map;
-
 public class Trie {
-    private TrieNode root;
+
+    // refer to leetCode.M_Trie_208
 }
 
-class TrieNode{
-    private String value;
-    private Map<Character, TrieNode> children;
-    private boolean isWord;
-
-    TrieNode(String value){
-        this.value = value;
-    }
-
-}
--- Java_Exercise/src/java/leetCode/M_BitManipulation_Trie_421.java
+++ Java_Exercise/src/java/leetCode/M_BitManipulation_Trie_421.java
@@ -26,17 +26,94 @@ public class M_BitManipulation_Trie_421 {
     public static void main(String[] args) {
 
         int[] b = {5, 25};  // 28
-        System.out.println(findMaximumXOR1(b));
+        System.out.println(findMaximumXOR(b));
 
-//        int[] c = {3, 10, 5, 25, 2, 8};  // 28
-//        System.out.println(findMaximumXOR1(c));
-//
-//        int[] a = {3, 32, 5, 25, 10}; //57
-//        System.out.println(findMaximumXOR1(a));
+        int[] c = {3, 10, 5, 25, 2, 8};  // 28
+        System.out.println(findMaximumXOR(c));
+
+        int[] a = {3, 32, 5, 25, 10}; //57
+        System.out.println(findMaximumXOR(a));
     }
 
+    // Trie
+    static class TrieNode {
+        int val;
+        TrieNode leftChild; // 0
+        TrieNode rightChild; // 1
+
+        TrieNode(int val) {
+            this.val = val;
+        }
+    }
+
+    // https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/discuss/130427/()-92
     public static int findMaximumXOR(int[] nums) {
-        return 0;
+
+        // build the tree, storing binary form nums
+        // 0 goes to left, 1 goes to right
+        TrieNode root = new TrieNode(0);
+        TrieNode current = root;
+
+        for (int num : nums) {
+            for (int i = 31; i >= 0; --i) {
+                // check xor bit by bit, from right to left
+                // check from left to right does not work, since when we retain number that differs on lower digit,
+                // we could lost number that differ on higher digits, so we won't find the max XOR
+                int bit = num & (1 << i);
+
+                if (bit == 0) {
+                    if (current.leftChild == null) {
+                        current.leftChild = new TrieNode(0);
+                    }
+
+                    current = current.leftChild;
+                } else {
+                    if (current.rightChild == null) {
+                        current.rightChild = new TrieNode(1);
+                    }
+
+                    current = current.rightChild;
+                }
+            }
+
+            current = root;
+        }
+
+        // find max XOR
+        int maxXOR = 0;
+
+        for (int num : nums) {
+            int xor = 0;
+
+            for (int i = 31; i >= 0; --i) {
+                int bit = num & (1 << i);
+
+                // if the bit is 0, best is to go to rightChild(1)
+                if (bit == 0) {
+                    if (current.rightChild != null) {
+                        xor = xor | (1 << i);
+                        current = current.rightChild;
+                    } else {
+                        xor = xor | (0 << i);
+                        current = current.leftChild;
+                    }
+                } else {
+                    if (current.leftChild != null) {
+                        xor = xor | (1 << i);
+                        current = current.leftChild;
+                    } else {
+                        xor = xor | (0 << i);
+                        current = current.rightChild;
+                    }
+                }
+
+            }
+
+            maxXOR = maxXOR > xor ? maxXOR : xor;
+            current = root;
+        }
+
+        return maxXOR;
     }
 
     // Bit manipulation
@@ -47,13 +124,15 @@ public class M_BitManipulation_Trie_421 {
         int mask = 0;
 
         // Since we need largest XOR, we start from the very beginning, greedy approach
+        // iterate from 0 to 31 does not work, since when we retain number that differs on lower digit,
+        // we could lost number that differ on higher digits, so we won't find the max XOR
         for (int i = 31; i >= 0; --i) {
             mask = mask | (1 << i);  // each time, mask cover 1 bit, from left to right
 
             Set<Integer> set = new HashSet<>();
             for (int num : nums) {
-                int inspectingDigits = num & mask;
-                set.add(inspectingDigits);
+                int bits = num & mask;
+                set.add(bits);
             }
 
             // for current iteration, the possible maxXOR is greedyBet
@@ -65,8 +144,8 @@ public class M_BitManipulation_Trie_421 {
             // since a xor b = c   ⇒   a xor c = b   and    b xor c = a
             // so inspectingDigits ^ greedy = target
             // if target exists in set, then greedyBet is possible to get
-            for (int inspectingDigits : set) {
-                int target = inspectingDigits ^ greedyBet;
+            for (int bits : set) {
+                int target = bits ^ greedyBet;
 
                 if (set.contains(target)) {
                     maxXOR = greedyBet;
@@ -78,5 +157,4 @@ public class M_BitManipulation_Trie_421 {
 
         return maxXOR;
     }
-
 }
