e619fdd7dbbf75690102f25e3cf77b3cc5480333
ethan-cao <ethancaocgx@gmail.com>
2019-06-05 19:23:35 +0000

    date

--- Java_Exercise/src/java/leetCode/M_Trie_676.java
+++ Java_Exercise/src/java/leetCode/M_Trie_676.java
@@ -1,7 +1,36 @@
 package leetCode;
 
-public class M_Trie_676 {
-    public static void main(String[] args){
-	}
+/*
+Given a string, your task is to count how many palindromic substrings in this string.
 
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+
+### Example
+"abc" ->  3, Explanation: Three palindromic strings: "a", "b", "c".
+"aaa" ->  6, Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+### Condition
+The input string length won't exceed 1000.
+
+### Essential problem
+
+### Corner case
+
+*/
+public class M_Trie_676 {
+    public static void main(String[] args) {
+
+        System.out.println("abc");  // 3
+        System.out.println("aaa");  // 6
+    }
+
+    public static int countSubstrings(String s) {
+        int result = 0;
+
+
+
+        return result;
+    }
 }
+
