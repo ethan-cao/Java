897acbd2498e2dbeb841ff1b8683b74c80aaf229
ethan-cao <ethancaocgx@gmail.com>
2019-06-12 19:09:10 +0000

    counting sort

--- Java_Exercise/src/java/algorithm/sorting/CountingSort.java
+++ Java_Exercise/src/java/algorithm/sorting/CountingSort.java
@@ -1,9 +1,42 @@
 package algorithm.sorting;
 
-public class CountingSort {
-    public static void main(String[] args){
-        int[] data = {2, 4, 5, 3, 1};
-        int l = data.length;
+import java.util.Arrays;
 
+/**
+ * Valid for integer only and need to know the range of our input integers.
+ * works better with same rang integers (max-mix)
+ */
+
+public class CountingSort {
+
+    public static void main(String[] args) {
+        int[] data = {2, 4, 5, 4, 2, 3, 1};
+
+        sort(data, 1, 5);
+
+        System.out.println(Arrays.toString(data));
+    }
+
+    public static void sort(int[] array, int smallest, int largest) {
+        // count occurrence
+        int[] tally = new int[largest - smallest + 1];
+        for (int a : array) {
+            tally[a - smallest] += 1;
+        }
+
+        // after iteration, tally[i] embodies number of elements that are <=  (the element represented by tally[i])
+        for (int i = 1; i < tally.length; ++i) {
+            tally[i] += tally[i - 1];
+        }
+
+        // position element in order
+        int[] result = new int[array.length];
+        for (int a : array) {
+            int numberOfSmallerElements = tally[a - smallest]; // number of elements that are <=  (the element represented by tally[i])
+            result[numberOfSmallerElements - 1] = a;  // -1 : since index starts from 0
+            tally[a - smallest]--;
+        }
+
+        System.arraycopy(result, 0, array, 0, array.length);
     }
 }
--- Java_Exercise/src/java/algorithm/sorting/RadixSort.java
+++ Java_Exercise/src/java/algorithm/sorting/RadixSort.java
@@ -1,5 +1,18 @@
 package algorithm.sorting;
 
+/**
+ * Radix sort handles sorting by implementing counting sort (or bucket sort) on one digit at a time.
+ *  It generally works best for a range of smallish numbers,
+ *  the very first sorting algorithm to be created
+ *
+ *  complexity is O(wn), n number of elements, w number of digit of the largest element
+ *
+ *  Rarely use since time complexity, LSD is more often than MSD
+ *
+ *  1. most significant digit (MSD) radix sort :  from the greatest digit, and moving towards the least significant digit
+ *  2. least significant digit (LSD) radix sort : the smallest digit first, moving towards the greater
+ */
+
 public class RadixSort {
     public static void main(String[] args){
         int[] data = {2, 4, 5, 3, 1};
