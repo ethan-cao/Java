cf135b5aaed042313c309253d5d0d87d0188f2fb
guane <Ethan.GuangxueCao@pega.com>
2019-06-27 19:31:57 +0000

    udpate

--- Java_Exercise/src/java/algorithm/leetCode/A_M_Array_56.java
+++ Java_Exercise/src/java/algorithm/leetCode/A_M_Array_56.java
@@ -14,7 +14,7 @@ Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 
 */
 
-public class M_Array_56 {
+public class A_M_Array_56 {
 
     public static void main(String... args) {
 
