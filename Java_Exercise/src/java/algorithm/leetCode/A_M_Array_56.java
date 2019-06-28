a004e32a6ae5af807bcb70fb3bd0f03fb336ffdd
guane <Ethan.GuangxueCao@pega.com>
2019-06-28 13:33:46 +0000

    56 done

--- Java_Exercise/src/java/algorithm/leetCode/A_M_Array_56.java
+++ Java_Exercise/src/java/algorithm/leetCode/A_M_Array_56.java
@@ -3,7 +3,6 @@ package algorithm.leetCode;
 /*
 Given a collection of intervals, merge all overlapping intervals
 
-input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
 
 ### Example
 [[1,3],[2,6],[8,10],[15,18]] --> [[1,6],[8,10],[15,18]]
@@ -14,13 +13,63 @@ Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 
 */
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class A_M_Array_56 {
 
     public static void main(String... args) {
+//        int[][] intervals = {
+//                {1, 3},
+//                {2, 6},
+//                {8, 10},
+//                {15, 18}
+//        };
 
+        int[][] intervals = {
+                {1, 4},
+                {5, 6},
+                {2, 7},
+        };
+
+
+        int[][] mergedIntervals = merge(intervals);
+
+        for (int[] merge : mergedIntervals) {
+            System.out.println(Arrays.toString(merge));
+        }
     }
 
     public static int[][] merge(int[][] intervals) {
-        return null;
+        if (intervals == null) {
+            return null;
+        }
+
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> mergedIntervals = new ArrayList();
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int[] interval : intervals) {
+            if (end >= interval[0]) {
+                end = Math.max(end, interval[1]);
+            } else {
+                mergedIntervals.add(new int[]{start, end});
+                start = interval[0];
+                end = interval[1];
+            }
+
+        }
+        mergedIntervals.add(new int[]{start, end});
+
+        return mergedIntervals.toArray(new int[0][]);
     }
 }
