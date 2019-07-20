0a33632253fea7505d4dcaf476e944d919d841af
ethan-cao <ethancaocgx@gmail.com>
2019-07-20 18:41:55 +0200

    new 139

---
+++ Java_Exercise/src/java/algorithm/leetCode/M_DP_Array_139.java
@@ -0,0 +1,39 @@
+package algorithm.leetCode;
+
+/*
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+
+### Example
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+             Note that you are allowed to reuse a dictionary word.
+
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false
+
+*/
+
+
+import java.util.List;
+
+public class M_DP_Array_139 {
+
+    public static void main(String... args) {
+
+    }
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        boolean result = false;
+
+        return result;
+    }
+}
