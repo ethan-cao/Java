e9de7ec70bef9626954330593d1b84ad196a0dbf
ethan-cao <ethancaocgx@gmail.com>
2019-08-11 22:09:56 +0200

    update

--- Java_Exercise/src/java/algorithm/leetCode/M_DP_64.java
+++ Java_Exercise/src/java/algorithm/leetCode/M_DP_64.java
@@ -27,19 +27,22 @@ public class M_DP_64 {
                 {1, 5, 1},
                 {4, 2, 1}
         };
-        System.out.println(minPathSum(grid)); // 7
+        System.out.println(minPathSum1(grid)); // 7
 
         int[][] grid1 = {
                 {12, 3, 1},
                 {1, 5, 1}
         };
-        System.out.println(minPathSum(grid1)); // 17
+        System.out.println(minPathSum1(grid1)); // 17
     }
 
+    // Recursive, use tree to analyze, DFS
+    // get sum of each path towards each leaf
     public static int minPathSum(int[][] grid) {
         return getPathSum(grid, 0, 0);
     }
 
+    // return minimal sum from grid[i][j] to all its leaves
     private static int getPathSum(int[][] grid, int i, int j) {
         int current = grid[i][j];
 
@@ -62,15 +65,56 @@ public class M_DP_64 {
     // Suppose the minimum path sum of arriving at point (i, j) is S[i][j],
     // then the state equation is S[i][j] = min(S[i - 1][j], S[i][j - 1]) + grid[i][j].
     public static int minPathSum1(int[][] grid) {
-        // pathSum from (0,0) to (i, j)
-        int[][] pathSum = new int[grid.length][grid[0].length];
+        int m = grid.length;
+        int n = grid[0].length;
 
-        for (int i = 0; i < grid.length; ++i) {
-            for (int j = 0; j < grid[0].length; ++j) {
-                pathSum[i][j] = grid[i][j];
+        int[][] minSum = new int[m][n];
+        minSum[0][0] = grid[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j > 0) {
+                    minSum[i][j] = minSum[i][j - 1] + grid[i][j];
+                }
+
+                if (j == 0 && i > 0) {
+                    minSum[i][j] = minSum[i - 1][j] + grid[i][j];
+                }
+
+                if (i > 0 && j > 0) {
+                    minSum[i][j] = Math.min(minSum[i][j - 1], minSum[i - 1][j]) + grid[i][j];
+                }
             }
         }
 
-        return 1;
+        return minSum[m - 1][n - 1];
+    }
+
+    // Iterative with 1-d array
+    public static int minPathSum2(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[] minSum = new int[n];
+        minSum[0] = grid[0][0];
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+
+                if (i == 0 && j > 0) {
+                    minSum[j] = minSum[j - 1] + grid[i][j];
+                }
+
+                if (j == 0 && i > 0) {
+                    minSum[j] = minSum[j] + grid[i][j];
+                }
+
+                if (i > 0 && j > 0) {
+                    minSum[j] = Math.min(minSum[j], minSum[j - 1]) + grid[i][j];
+                }
+            }
+        }
+
+        return minSum[n - 1];
     }
 }
