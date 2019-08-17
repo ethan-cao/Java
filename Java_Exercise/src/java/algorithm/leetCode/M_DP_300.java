bea7cbe452ee8eb0141eb3f1d3d5866bd492cc23
ethan-cao <ethancaocgx@gmail.com>
2019-08-17 23:29:28 +0200

    update

--- Java_Exercise/src/java/algorithm/leetCode/M_DP_300.java
+++ Java_Exercise/src/java/algorithm/leetCode/M_DP_300.java
@@ -12,17 +12,43 @@ Follow up: Could you improve it to O(n log n) time complexity?
 [10,9,2,5,3,7,101,18] -> 4
 Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
 
+### sub problem
+
+length of longest increasing subsequence for {a1}, {a1, a2}, {a1, a2} ...
+
 */
 
 
+import java.util.Arrays;
+
 public class M_DP_300 {
     public static void main(String... args) {
-        int[] nums1 = {10,9,2,5,3,7,101,18};
+        int[] nums1 = {10, 9, 2, 5, 3, 7, 101, 18};
         System.out.println(lengthOfLIS(nums1)); // 4
     }
 
     public static int lengthOfLIS(int[] nums) {
-        return 1;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int maxLength = 0;
+
+        // length[i]: length of longest increasing subsequence for num[0...i]
+        int[] length = new int[nums.length];
+        Arrays.fill(length, 1);
+
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                if (nums[j] < nums[i]) {
+                    length[i] = Math.max(length[j] +1, length[i]);
+                }
+            }
+
+            maxLength = Math.max(maxLength, length[i]);
+        }
+
+        return maxLength;
     }
 
 }
