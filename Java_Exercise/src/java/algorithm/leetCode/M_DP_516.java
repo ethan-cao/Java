711b188f64ddab64475c763a4daa3d0afbd96da3
guane <Ethan.GuangxueCao@pega.com>
2019-08-21 08:54:22 +0000

    update

--- Java_Exercise/src/java/algorithm/leetCode/M_DP_516.java
+++ Java_Exercise/src/java/algorithm/leetCode/M_DP_516.java
@@ -7,15 +7,23 @@ You may assume that the maximum length of s is 1000.
 ### Example
 "bbbab" -> 4, One possible longest palindromic subsequence is "bbbb".
 "cbbd" -> 2, One possible longest palindromic subsequence is "bb".
+
+
+
+Related: 5, 647
 */
 
 
 public class M_DP_516 {
     public static void main(String... args) {
-
+        System.out.println(longestPalindromeSubseq("")); //
+        System.out.println(longestPalindromeSubseq("a")); //
+        System.out.println(longestPalindromeSubseq("aa")); //
+        System.out.println(longestPalindromeSubseq("bbbab")); // 4
+        System.out.println(longestPalindromeSubseq("cbbd")); // 2
     }
 
-    public int longestPalindromeSubseq(String s) {
+    public static int longestPalindromeSubseq(String s) {
         return 1;
     }
 
