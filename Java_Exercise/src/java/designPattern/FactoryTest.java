a374cd5e9e8ce6d9ba977c20ad2b1d92bfe04f0c
guane <Ethan.GuangxueCao@pega.com>
2019-09-02 14:33:15 +0000

    update

--- Java_Exercise/src/java/designPattern/FactoryTest.java
+++ Java_Exercise/src/java/designPattern/FactoryTest.java
@@ -7,49 +7,49 @@ package designPattern;
  */
 
 public class FactoryTest {
-	public static void main(String[] args) {
-		ShapeFactory shapeFactory = new ShapeFactory();
-		Shape shape;
-		
-		shape =shapeFactory.getShape("Circle");
-		System.out.println(shape);
-		shape= shapeFactory.getShape("Rectangle");
-		System.out.println(shape);
-	}
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape shape;
+
+        shape = shapeFactory.getShape("Circle");
+        System.out.println(shape);
+        shape = shapeFactory.getShape("Rectangle");
+        System.out.println(shape);
+    }
 }
 
 interface Shape {
-	void draw();
+    void draw();
 }
 
 class Rectangle implements Shape {
-   public void draw() {
-      System.out.println("This is a Rectangle.");
-   }
+    public void draw() {
+        System.out.println("This is a Rectangle.");
+    }
 }
 
 class Circle implements Shape {
-   public void draw() {
-      System.out.println("This is a Circle");
-   }
+    public void draw() {
+        System.out.println("This is a Circle");
+    }
 }
 
 class ShapeFactory {
-   public Shape getShape(String shapeType){
-      if(shapeType == null){
-         return null;
-      }		
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
 
-      if(shapeType.equalsIgnoreCase("CIRCLE")){
-         return new Circle();
-      } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
-         return new Rectangle();
-      }
-      
-      // if we need to add more shapes, we have to modify this ShapeFactory 
-      // This is against open for extension, but closed for modification.
-      // That is why Factory Method comes to here 
-      
-      return null;
-   }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        }
+
+        // if we need to add more shapes, we have to modify this ShapeFactory
+        // This is against open for extension, but closed for modification.
+        // That is why Factory Method comes to here
+
+        return null;
+    }
 }
