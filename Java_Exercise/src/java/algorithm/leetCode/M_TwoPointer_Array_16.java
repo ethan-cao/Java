ca7ac3f9e6839f572af06728fca79feb3ce834e4
guane <Ethan.GuangxueCao@pega.com>
2019-09-03 19:07:38 +0000

    update

---
+++ Java_Exercise/src/java/algorithm/leetCode/M_TwoPointer_Array_16.java
@@ -0,0 +1,28 @@
+package algorithm.leetCode;
+
+/*
+Given an array nums of n integers and an integer target,
+find three integers in nums such that the sum is closest to target. Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+### Example
+Given array nums = [-1, 2, 1, -4], and target = 1.
+
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+*/
+
+
+public class M_TwoPointer_Array_16 {
+
+    public static void main(String... args) {
+        System.out.println(threeSumClosest(new int[]{-1, 2, 1, -4}, 1));  // 2
+    }
+
+    public static int threeSumClosest(int[] nums, int target) {
+        int closestSum = 0;
+
+        return closestSum;
+    }
+
+}
