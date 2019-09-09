e78b2228758f54ac519042bf0b79fd17b0f5dfba
ethan-cao <ethancaocgx@gmail.com>
2019-09-09 23:35:07 +0200

    update

--- Java_Exercise/src/java/algorithm/leetCode/M_Array_34.java
+++ Java_Exercise/src/java/algorithm/leetCode/M_Array_34.java
@@ -24,17 +24,35 @@ public class M_Array_34 {
     public static void main(String... args) {
         System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8))); //[3,4]
         System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6))); //[-1,-1]
+        System.out.println(Arrays.toString(searchRange(new int[]{1}, 1))); //[0,0]
+        System.out.println(Arrays.toString(searchRange(new int[]{1, 3}, 1))); //[0,0]
+        System.out.println(Arrays.toString(searchRange(new int[]{1, 4}, 4))); //[1,1]
+        System.out.println(Arrays.toString(searchRange(new int[]{1, 2, 3}, 1))); //[0,0]
     }
 
     // O(log n)
     public static int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+
+        if (nums.length == 1 && nums[0] == target) {
+            return new int[]{0, 0};
+        }
+
         int[] result = {-1, -1};
+
+        if (nums.length == 2) {
+            result[0] = nums[0] == target ? 0 : nums[1] == target ? 1 : -1;
+            result[1] = nums[0] == target ? nums[1] == target ? 1 : 0 : result[0] == 1 ? 1 : -1;
+            return result;
+        }
 
         int start = 0;
         int end = nums.length - 1;
         int middle = start + (end - start) / 2;
 
-        while (middle >= 0 && middle < nums.length) {
+        while (middle >= 0 && middle < nums.length && end - start > 1) {
 
             if (nums[middle] == target) {
                 int left = middle;
@@ -58,6 +76,7 @@ public class M_Array_34 {
             }
 
             middle = start + (end - start) / 2;
+
         }
 
         return result;
