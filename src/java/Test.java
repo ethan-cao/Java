46ee9bfd495ee42363063323738373855829f6d2
ethan-cao <ethancaocgx@gmail.com>
2019-09-22 20:57:10 +0000

    update

--- src/java/Test.java
+++ src/java/Test.java
@@ -4,18 +4,6 @@ public class Test {
 
     public static void main(String... args) {
 
-        List<List<Integer>> c = new ArrayList<>();
-
-        c.add(Arrays.asList(1, 2));
-        c.add(Arrays.asList(3, 4));
-
-        for(List<Integer> list: c){
-            list.add(0, 0);
-        }
-
-
-
-        System.out.println(c.get(0));
     }
 
 }
--- src/java/algorithm/leetCode/M_Array_48.java
+++ src/java/algorithm/leetCode/M_Array_48.java
@@ -49,13 +49,65 @@ public class M_Array_48 {
                 {15, 14, 12, 16}
         };
 
-        rotate(matrix);
+        rotate1(matrix);
 
         for (int[] merge : matrix) {
             System.out.println(Arrays.toString(merge));
         }
     }
 
+    // time : O(N^2)
     public static void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+
+        int L = matrix.length;
+
+        // transpose matrix : A[i][j] = A[j][i]
+        for (int x = 0; x < L; ++x) {
+            for (int y = 0; y <= x; ++y) {
+                swap(matrix, x, y, y, x);
+            }
+        }
+
+        // flip matrix horizontally
+        // flip matrix vertically, for anticlockwise rotation,
+        for (int x = 0; x < L; ++x) {
+            for (int y = 0; y < L / 2; ++y) {
+                swap(matrix, x, y, x, L - 1 - y);
+            }
+        }
+    }
+
+    private static void swap(int[][] matrix, int x1, int y1, int x2, int y2) {
+        int temp = matrix[x1][y1];
+        matrix[x1][y1] = matrix[x2][y2];
+        matrix[x2][y2] = temp;
+    }
+
+    // https://leetcode.com/problems/rotate-image/discuss/18895/Clear-Java-solution/152227
+    public static void rotate1(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+
+        int L = matrix.length;
+
+        for (int x = 0; x < L / 2; ++x) {
+            for (int y = 0; y < L - 1 - x; ++y) {
+                rotate(matrix, x, y);
+            }
+        }
+    }
+
+    private static void rotate(int[][] matrix, int x, int y) {
+        int L = matrix.length;
+
+        int temp = matrix[x][y];
+        matrix[x][y] = matrix[L - 1 - y][x];
+        matrix[L - 1 - y][x] = matrix[L - 1 - y][L - 1 - x];
+        matrix[L - 1 - y][L - 1 - x] = matrix[y][L - 1 - x];
+        matrix[y][L - 1 - x] = temp;
     }
 }
