c4099551cde305e5853fcc058e0309d0806dbc82
ethan-cao <ethancaocgx@gmail.com>
2019-09-29 10:31:04 +0000

    update

---
+++ src/java/algorithm/leetCode/M_Array_59.java
@@ -0,0 +1,35 @@
+package algorithm.leetCode;
+
+/*
+Given a positive integer n, generate a square matrix filled with elements from 1 to n^2 in spiral order.
+
+### Example
+Input: 3
+Output:
+[
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+]
+*/
+
+
+public class M_Array_59 {
+
+    public static void main(String... args) {
+        int[][] matrix1 = generateMatrix(2);
+        int[][] matrix2 = generateMatrix(3);
+        int[][] matrix3 = generateMatrix(4);
+
+
+    }
+
+    public static int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+
+
+
+        return matrix;
+    }
+
+}
---
+++ src/java/algorithm/leetCode/M_DP_Greedy_Array_55.java
@@ -0,0 +1,67 @@
+package algorithm.leetCode;
+
+/*
+Given an array of non-negative integers, you are initially positioned at index 0
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+
+### Example
+[2,3,1,1,4] ->  true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+[3,2,1,0,4] ->  false
+Explanation: always arrive at index 3. Its maximum jump length is 0, which makes it impossible to reach the last index.
+*/
+
+
+public class M_DP_Greedy_Array_55 {
+
+    public static void main(String... args) {
+        System.out.println(canJump(new int[]{2, 3, 1, 1, 4})); // T
+        System.out.println(canJump(new int[]{3, 2, 1, 0, 4})); // F
+        System.out.println(canJump(new int[]{2, 3, 1, 31, 1})); // T
+    }
+
+    // Greedy, 1ms
+    public static boolean canJump(int[] nums) {
+        int maxJumpIdx = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (maxJumpIdx < i) {
+                // if the max jump index cannot reach index i, not possible to proceed
+                return false;
+            }
+
+            maxJumpIdx = Math.max(maxJumpIdx, i + nums[i]); // greedy
+        }
+        // possible to reach index nums.length -1
+
+        return true;
+    }
+
+    // DP, 200ms
+    public static boolean canJump1(int[] nums) {
+        // if canJumpToEnd[i] is true; we can jump from index i to nums.length-1
+        // we need to get canJumpToEnd[0];
+        boolean[] canJump = new boolean[nums.length];
+        canJump[nums.length - 1] = true;
+
+        for (int idx = nums.length - 2; idx >= 0; --idx) {
+            for (int step = 0; step <= nums[idx]; ++step) {
+
+                if (idx + step >= nums.length - 1) {
+                    canJump[idx] = true;
+                } else {
+                    canJump[idx] = canJump[idx + step];
+                }
+
+                if (canJump[idx]) {
+                    break;
+                }
+            }
+        }
+
+        return canJump[0];
+    }
+
+}
