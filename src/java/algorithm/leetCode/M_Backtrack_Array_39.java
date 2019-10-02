743c0abb5997d70f844f0a53d98963cd41b07f59
ethan-cao <ethancaocgx@gmail.com>
2019-10-02 10:09:11 +0000

    update

--- src/java/algorithm/leetCode/M_Backtrack_Array_39.java
+++ src/java/algorithm/leetCode/M_Backtrack_Array_39.java
@@ -1,20 +1,19 @@
 package algorithm.leetCode;
 
 /*
-Given a collection of candidate numbers (candidates) and a target number (target),
+Given a set of candidate numbers (candidates) (without duplicates) and a target number (target),
 find all unique combinations in candidates where the candidate numbers sums to target.
-
-Each number in candidates may only be used once in the combination.
+The same repeated number may be chosen from candidates unlimited number of times.
 
 All numbers (including target) will be positive integers.
 The solution set must not contain duplicate combinations.
 
 ### Example
-Input: candidates = [10,1,2,7,6,1,5], target = 8,
-A solution set is: [[1,1,6],[1,2,5],[1,7],[2,6]]
+Input: candidates = [2,3,6,7], target = 7,
+A solution set is: [  [7],  [2,2,3] ]
 
-Input: candidates = [2,5,2,1,2], target = 5,
-A solution set is: [ [1,2,2], [5] ]
+Input: candidates = [2,3,5], target = 8,
+A solution set is: [  [2,2,2,2],  [2,3,3],   [3,5] ]
 
 */
 
@@ -22,47 +21,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class M_Backtrack_Array_40 {
+public class M_Backtrack_Array_39 {
 
     public static void main(String... args) {
-        System.out.println(combinationSum(new int[]{8, 8}, 8)); // [[8]]
-        System.out.println(combinationSum(new int[]{2, 5, 2, 1, 2}, 5)); // [ [1,2,2], [5] ]
-        System.out.println(combinationSum(new int[]{10, 1, 2, 7, 6, 1, 5}, 8)); // [[1,1,6],[1,2,5],[1,7],[2,6]]
+        int[] candidates = {1, 1, 2};
+
+        List<List<Integer>> results = combinationSum(candidates, 7);
+        for (List<Integer> result : results) {
+            System.out.println(Arrays.toString(result.toArray()));
+        }
+        // [  [7],  [2,2,3] ]
+
     }
 
-    // Time complexity O(2^n). Space complexity O(n)
+    // Backtrack
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
-
-        List<List<Integer>> combinations = new ArrayList<>();
-        List<Integer> combination = new ArrayList<>();
-
-        getCombinations(candidates, 0, target, combination, combinations);
-
-        return combinations;
-    }
-
-    private static void getCombinations(int[] candidates, int start, int target, List<Integer> combination, List<List<Integer>> combinations) {
-        if (target == 0) {
-            combinations.add(new ArrayList<>(combination));  // pick combination that sum up to target
-            return;
-        }
-
-        for (int i = start; i < candidates.length; ++i) {
-            if (candidates[i] > target) {
-                continue;
-            }
-
-            if (i > start && candidates[i] == candidates[i - 1]) {  // !!! i > start
-                continue; // skip duplicate candidate
-            }
-
-            combination.add(combination.size(), candidates[i]);  // Try
-
-            getCombinations(candidates, start + 1, target - candidates[i], combination, combinations);
-
-            combination.remove(combination.size() - 1);   // Backtrack
-        }
+        return null;
     }
 
 }
--- src/java/algorithm/leetCode/M_Backtrack_Array_47.java
+++ src/java/algorithm/leetCode/M_Backtrack_Array_47.java
@@ -23,12 +23,72 @@ public class M_Backtrack_Array_47 {
             System.out.println(Arrays.toString(subset.toArray()));
         }
         // [  [1,1,2],   [1,2,1],   [2,1,1] ]
-
     }
 
     // Backtrack
     public static List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> permutations = new ArrayList<>();
+        List<Integer> tracker = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
 
-        return null;
+        Arrays.sort(nums);
+        collectPermutation(nums, permutations, tracker, used);
+
+        return permutations;
     }
+
+    private static void collectPermutation(int[] nums, List<List<Integer>> permutations, List<Integer> tracker, boolean[] used) {
+        if (tracker.size() == nums.length) {
+            permutations.add(new ArrayList<>(tracker));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (used[i]) {
+                continue;
+            }
+
+            // skip duplication, require nums to be sorted
+            // !used[i-1] : the previous one has been use in previous round
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+
+            tracker.add(nums[i]);
+            used[i] = true;
+            collectPermutation(nums, permutations, tracker, used);
+            tracker.remove(tracker.size() - 1);
+            used[i] = false;
+        }
+    }
+
+    // Iterative, slower than backtrack
+    public static List<List<Integer>> permuteUnique1(int[] nums) {
+        List<List<Integer>> permutations = new ArrayList<>();
+        permutations.add(new ArrayList<>());
+
+        for (int i = 0; i < nums.length; ++i) {
+            int size = permutations.size();
+
+            // clear all existing permutations, replace with newPermutation
+            for (int j = 0; j < size; ++j) {
+                List<Integer> permutation = permutations.remove(0);
+
+                for (int insertIdx = 0; insertIdx <= permutation.size(); ++insertIdx) {
+
+                    // if
+                    if (insertIdx > 0 && nums[i] == permutation.get(insertIdx - 1)) {
+                        break;
+                    }
+
+                    ArrayList<Integer> newPermutation = new ArrayList<>(permutation);
+                    newPermutation.add(insertIdx, nums[i]);
+                    permutations.add(newPermutation);
+                }
+            }
+        }
+
+        return permutations;
+    }
+
 }
