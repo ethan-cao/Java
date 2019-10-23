e282f5e56cb40a069870cd87245d422ab10988eb
ethan-cao <ethancaocgx@gmail.com>
2019-10-23 16:23:28 +0000

    update

--- src/java/Test.java
+++ src/java/Test.java
@@ -5,16 +5,6 @@ import java.util.*;
 public class Test {
 
     public static void main(String... args) {
-    // his is typically implemented by converting the internal address of the object into an intege
-
-        Test t1 = new Test();
-        Test t2 = new Test();
-
-        System.out.println(t1.hashCode());
-        System.out.println(t2.hashCode());
-
-        System.out.printf("%s is good", "J");
-
     }
 
 }
--- src/java/algorithm/leetCode/M_Stack_String_856.java
+++ src/java/algorithm/leetCode/M_Stack_String_856.java
@@ -20,22 +20,60 @@ S is a balanced parentheses string, containing only ( and ).
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
 
 public class M_Stack_String_856 {
 
     public static void main(String... args) {
-        System.out.println(scoreOfParentheses("()"));        // 1
-        System.out.println(scoreOfParentheses("(())"));      // 2
-        System.out.println(scoreOfParentheses("()()"));      // 2
-        System.out.println(scoreOfParentheses("(()(()))"));  // 6
+        System.out.println(scoreOfParentheses1("()"));          // 1
+        System.out.println(scoreOfParentheses1("()()"));        // 2
+        System.out.println(scoreOfParentheses1("()()()"));      // 3
+        System.out.println(scoreOfParentheses1("(())"));        // 2
+        System.out.println(scoreOfParentheses1("((()))"));      // 4
+        System.out.println(scoreOfParentheses1("(()(()))"));    // 6
+        System.out.println(scoreOfParentheses1("((()))(())"));  // 6
+        System.out.println(scoreOfParentheses1("()(())()(((()(()())))(((())(()))()))(((())))"));  // 88
     }
 
-    static int scoreOfParentheses(String S) {
+    // Stack
+    // Time: O(N), Space: O(N)
+    public static int scoreOfParentheses(String s) {
         int score = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(score);
+                score = 0;
+            } else {
+                int previousLevelScore = stack.pop();
+                int currentLevelScore = Math.max(score * 2, 1);
+
+                score = currentLevelScore + previousLevelScore;
+            }
+        }
 
         return score;
     }
 
+    // Time: O(N), Space: O(1)
+    public static int scoreOfParentheses1(String s) {
+        int score = 0;
+        int level = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                level++;
+            } else {
+                level--;
+            }
+
+            if (c == ')' && s.charAt(i - 1) == '(') {
+                score += 1 << level;
+            }
+        }
+
+        return score;
+    }
 }
