c04404faca03c764bd631b5fd045e0c98ffcb0ea
ethan-cao <ethancaocgx@gmail.com>
2019-10-26 15:48:58 +0000

    update

--- src/java/algorithm/leetCode/M_TwoPointer_Array_209.java
+++ src/java/algorithm/leetCode/M_TwoPointer_Array_209.java
@@ -12,66 +12,78 @@ Explanation: the subarray [4,3] has the minimal length under the problem constra
 
 */
 
-public class M_TwoPointer_SlidingWindow_Array_209 {
+public class M_TwoPointer_Array_209 {
 
     public static void main(String... args) {
         System.out.println(minSubArrayLen1(7, new int[]{2, 3, 1, 2, 4, 3}));  // 2
-        System.out.println(minSubArrayLen1(3, new int[]{1, 1}));  // 0
-        System.out.println(minSubArrayLen1(7, new int[]{1, 1, 7, 1, 1}));  // 1
-        System.out.println(minSubArrayLen1(11, new int[]{1, 2, 3, 4, 5}));  // 3
+        System.out.println(minSubArrayLen1(3, new int[]{1, 1}));              // 0
+        System.out.println(minSubArrayLen1(7, new int[]{1, 1, 7, 1, 1}));     // 1
+        System.out.println(minSubArrayLen1(11, new int[]{1, 2, 3, 4, 5}));    // 3
     }
 
-    // Time: O(n)
+    // Time: O(N)
     // in the worst case, pointer right moves forward n times and pointer left also moves forward n times.
     public static int minSubArrayLen(int s, int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
 
-        int minLength = nums.length;
         boolean hasMinLength = false;
+        int minLength = nums.length;
         int sum = 0;
 
         // since we are looking for subarray length, naturally think about using 2 pointer
         // subarray means contiguous elements, use left and right index narrow the window
-        // you cannot sort as the current order actually matters
-        for (int left = 0, right = 0; right < nums.length; ++right) {
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
             sum += nums[right];
 
+            // since nums only contains positive, sums is strictly increasing
+            // no need e to include more elements once sum >= s
             while (sum >= s) {
                 hasMinLength = true;
-
-                // length of current subarray is (rigth - left + 1)
                 minLength = Math.min(minLength, right - left + 1);
 
+                if (minLength == 1) {
+                    return 1; // 1 is the shortest possible case
+                }
+
+                // try moving left, see if we can shorted length with sun >= s
                 sum -= nums[left];
                 left++;
             }
+
+            right++;
         }
 
         return hasMinLength ? minLength : 0;
     }
 
     // Time: O(NlogN)
+    // logN remind one of binary search
     public static int minSubArrayLen1(int s, int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
 
-        // since nums only contains positive, sums is strictly increasing
+        // since nums only contains positive, sums[] is strictly increasing (sorted)
+        // sum[i] is sum from nums[0] to nums[i] (inclusive)
         int[] sums = new int[nums.length + 1];
         for (int i = 1; i < sums.length; ++i) {
             sums[i] = sums[i - 1] + nums[i - 1];
         }
 
-        int minLength = nums.length;
         boolean hasMinLength = false;
+        int minLength = nums.length;
 
-        for (int left = 0; left < nums.length; ++left) {
-            // look for sums[left] + s, which is surely >= s
+        // given a left for sums[], right can be searched using binary search
+        for (int left = 0; left < sums.length; ++left) {
+            // if there is an index right exists between [left+1, sums.length-1]
+            // and the corresponding value is sums[left] + s, then sum between [left+1, right] is s
             int right = search(sums, left + 1, sums.length - 1, sums[left] + s);
 
-            if (right != -1) {
+            if (right != sums.length) {
                 hasMinLength = true;
                 minLength = Math.min(minLength, right - left);
             }
@@ -80,23 +92,20 @@ public class M_TwoPointer_SlidingWindow_Array_209 {
         return hasMinLength ? minLength : 0;
     }
 
+    // Binary search
     private static int search(int[] nums, int left, int right, int target) {
+        // narrow the window to length 1
         while (left <= right) {
             int middle = left + (right - left) / 2;
 
-            if (nums[middle] == target) {
-                return middle;
-            }
-
-            if (nums[middle] > target) {
+            if (nums[middle] >= target) {
                 right = middle - 1;
             } else {
                 left = middle + 1;
             }
         }
 
-        return -1;
+        return left;
     }
 
 }
-
