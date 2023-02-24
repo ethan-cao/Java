f5703c907cd7e121e05bbf95ab6336d5371810fc
ethan-cao <ethancaocgx@gmail.com>
2023-02-24 09:05:23 +0000

    update

--- src/java/algorithm/leetCode/M_Backtrack_Array_40.java
+++ src/java/algorithm/leetCode/M_Backtrack_Array_40.java
@@ -25,26 +25,27 @@ import java.util.List;
 public class M_Backtrack_Array_40 {
 
     public static void main(String... args) {
-        System.out.println(combinationSum(new int[]{8, 8}, 8)); // [[8]]
-        System.out.println(combinationSum(new int[]{2, 5, 2, 1, 2}, 5)); // [ [1,2,2], [5] ]
-        System.out.println(combinationSum(new int[]{10, 1, 2, 7, 6, 1, 5}, 8)); // [[1,1,6],[1,2,5],[1,7],[2,6]]
+        System.out.println(combinationSum(new int[] { 8, 8 }, 8)); // [[8]]
+        System.out.println(combinationSum(new int[] { 2, 5, 2, 1, 2 }, 5)); // [ [1,2,2], [5] ]
+        System.out.println(combinationSum(new int[] { 10, 1, 2, 7, 6, 1, 5 }, 8)); // [[1,1,6],[1,2,5],[1,7],[2,6]]
     }
 
     // Time complexity O(2^n). Space complexity O(n)
+    // 2ms
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> combinations = new ArrayList<>();
+        List<Integer> tracker = new ArrayList<>();
+
         Arrays.sort(candidates);
 
-        List<List<Integer>> combinations = new ArrayList<>();
-        List<Integer> combination = new ArrayList<>();
-
-        getCombinations(candidates, 0, target, combination, combinations);
+        collect(combinations, tracker, candidates, 0, target);
 
         return combinations;
     }
 
-    private static void getCombinations(int[] candidates, int start, int target, List<Integer> combination, List<List<Integer>> combinations) {
+    private static void collect(List<List<Integer>> combinations, List<Integer> tracker, int[] candidates, int start, int target) {
         if (target == 0) {
-            combinations.add(new ArrayList<>(combination));  // pick combination that sum up to target
+            combinations.add(new ArrayList<>(tracker)); // pick combination that sum up to target
             return;
         }
 
@@ -53,15 +54,15 @@ public class M_Backtrack_Array_40 {
                 break;
             }
 
-            if (i > start && candidates[i] == candidates[i - 1]) {  // !!! i > start
+            if (i > start && candidates[i] == candidates[i - 1]) { // !!! i > start
                 continue; // skip duplicate candidate
             }
 
-            combination.add(combination.size(), candidates[i]);  // Try
+            tracker.add(tracker.size(), candidates[i]); // Try
 
-            getCombinations(candidates, i + 1, target - candidates[i], combination, combinations);
+            getCombinations(candidates, i + 1, target - candidates[i], tracker, combinations);
 
-            combination.remove(combination.size() - 1);   // Backtrack
+            tracker.remove(tracker.size() - 1); // Backtrack
         }
     }
 
